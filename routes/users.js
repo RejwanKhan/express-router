61224797de618328cf5edf92ca56969534e0fd76
rekhan <rekhan@expediagroup.com>
2023-02-23 11:02:51 +0000

    Created crud for user

--- routes/users.js
+++ routes/users.js
@@ -34,4 +34,46 @@ router.get("/:id", (req, res) => {
   }
 });
 
+router.post("/", (req, res) => {
+  const { name, age } = req.body;
+  if (name && age) {
+    users.push(req.body);
+  } else {
+    res.send("Could not create new user, does not have all the correct fields");
+  }
+});
+
+router.put("/:id", (req, res) => {
+  let { id } = req.params;
+  id = Number(id);
+  const User = users[id - 1];
+  const { name, age } = req.body;
+
+  const valid = () => res.status(201).send("User has been created");
+  if ((name, age)) {
+    User.name = name;
+    User.age = age;
+    valid();
+  } else if (name) {
+    User.name = name;
+    valid();
+  } else if (age) {
+    User.age = age;
+    valid();
+  } else {
+    res.send("User was not updated");
+  }
+});
+
+router.delete("/:id", (req, res) => {
+  let { id } = req.params;
+  id = Number(id);
+  if (id <= users.length) {
+    users.splice(id - 1, 1);
+    res.send("User has been deleted");
+  } else {
+    res.status(404).send("User could not be found");
+  }
+});
+
 module.exports = router;
